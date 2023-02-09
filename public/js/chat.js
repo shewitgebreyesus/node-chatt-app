bd50756d55b457d85c6ddd359e6afa7d04639a46
shewitgebreyesus <59758309+shewitgebreyesus@users.noreply.github.com>
2023-02-09 18:49:48 +0300

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,7 +7,7 @@ const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
-// Templates
+
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
