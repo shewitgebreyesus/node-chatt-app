2323c978ae29346d7ae53ff44e9f10063aad6463
shewitgebreyesus <59758309+shewitgebreyesus@users.noreply.github.com>
2023-02-09 18:49:35 +0300

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 const socket = io()
 
-// Elements
+
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
